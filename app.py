8b26be6765cf658e7c17c292ec1c7e84cbf5c421
kai gouthro <777603+kaigouthro@users.noreply.github.com>
2023-11-11 13:47:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -61,19 +61,13 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-@st.experimental_singleton
-def get_driver():
-    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-
-options = Options()
-options.add_argument('--disable-gpu')
-options.add_argument('--headless')
-
-driver = get_driver()
 
 class RenderedPage:
     def __init__(self):
-        self.driver = driver
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")  # Run the browser in headless mode
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=chrome_options)
+
 
     def get_rendered_page(self, url):
                 
